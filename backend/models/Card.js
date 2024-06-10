641f50ec31eef2890f0b5dc1c2eaa763959a2dab
CharLee Racine <charleeracine@yahoo.com>
2024-06-10 09:11:17 -0600

    add cardSchema and validations

---
+++ backend/models/Card.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const cardSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
---
+++ backend/validations/cards.js
@@ -0,0 +1,23 @@
+const { check } = require("express-validator");
+const handleValidationErrors = require('./handleValidationErrors');
+
+const validateCardInput = [
+    check('title')
+    .exists({checkFalsy:true})
+    .isLength({min: 1, max: 40})
+    .withMessage('Card must have title'),
+    check('body')
+    .exists({checkFalsy:true})
+    .isLength({min: 1, max: 250})
+    .withMessage('Body must not be empty'),
+    check('author')
+    .exists({checkFalsy:true})
+    .withMessage('Author is required'),
+    check('category')
+    .exists({checkFalsy:true})
+    .withMessage('Category is required'),
+
+    handleValidationErrors
+];
+
+module.exports = validateCardInput;
