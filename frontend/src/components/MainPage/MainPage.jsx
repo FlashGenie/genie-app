ba3c8e96316885c1a4c32eb7a26240101c37e699
Dharani Yedavelly <dharani1414@gmail.com>
2024-06-10 10:13:37 -0700

    add space between the navbar and main heading

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ function MainPage() {
     return(
         <>
             <div className="flex flex-col items-center font-sans">
-                <h1 className="font-bold w-3/4 text-[5rem] text-center leading-10">AI-Powered Flashcards <img src="../../public/flashcard.svg" className="inline"/> from your notes, made effortless</h1>
+                <h1 className="font-bold w-3/4 text-[5rem] text-center leading-10 pt-32">AI-Powered Flashcards <img src="../../public/flashcard.svg" className="inline"/> from your notes, made effortless</h1>
                 <h2 className="text-gray-600 pt-10 pb-5">Create, Study, Explore, and Share: A Community of Flashcards</h2>
                 <button className="bg-black text-white px-3 py-1 rounded-md">Get started</button>
             </div>
