ae4fe61a5b5657817fd2ef027bd976765e38160a
Dharani Yedavelly <dharani1414@gmail.com>
2024-06-10 09:20:38 -0700

    get started button on the landing page

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -3,7 +3,8 @@ function MainPage() {
         <>
             <div className="flex flex-col items-center font-sans">
                 <h1 className="font-semibold w-3/4 text-7xl text-center">AI-Powered Flashcards from your notes, made effortless</h1>
-                <h2 className="text-gray-600 pt-5">Create, Study, Explore, and Share: A Community of Flashcards</h2>
+                <h2 className="text-gray-600 py-5">Create, Study, Explore, and Share: A Community of Flashcards</h2>
+                <button className="bg-black text-white px-3 py-1 rounded-md">Get started</button>
             </div>
         </>
     )
