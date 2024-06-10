0dfb8396e0ffe4bbc8f0c7b0161fab1326a5f493
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-06-10 07:47:08 -0700

    Update Main Page

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,10 +1,10 @@
 function MainPage() {
     return(
         <>
-            <p className="text-3xl font-bold underline">Genie - An AI Flash Card Generator at your Command</p>
-            <footer>
-                Copyright &copy; 2024 Genie
-            </footer>
+            <div className="w-full flex flex-col justify-center">
+                <h1 className="text-9xl">AI-Powered Flashcards from your notes, made effortless</h1>
+                <h2>Create, Study, Explore, and Share: A Community of Flashcards</h2>
+            </div>
         </>
     )
 }
