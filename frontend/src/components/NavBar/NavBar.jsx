ef2954810d4522a52e967314f9c08461db80283a
Jaspreet Singh <99jasingh@gmail.com>
2024-06-10 12:57:21 -0400

    Woking NavBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -15,44 +15,48 @@ function NavBar () {
     const getLinks = () => {
       if (loggedIn) {
         return (
-          <div className="links-nav">
-            <Link to={'/profile'}>Profile</Link>
-            <button onClick={logoutUser}>Logout</button>
+          <div className="links-nav flex items-center space-x-4">
+            <Link to="/profile" className="text-xl hover:text-slate-800 duration-500">Profile</Link>
+            <button onClick={logoutUser} className="text-xl hover:text-slate-800 duration-500">Logout</button>
           </div>
         );
       } else {
         return (
-          <div className="links-auth">
-            <Link to={'/signup'}>Signup</Link>
-            <Link to={'/login'}>Login</Link>
+          <div className="links-auth flex items-center space-x-4">
+            <Link to="/login" className="text-xl hover:text-slate-800 duration-500">Log in</Link>
+            <Link to="/signup" className="bg-black text-white font-sans duration-500 px-5 hover:bg-slate-800 rounded">
+              Get Started
+            </Link>
           </div>
         );
       }
     };
   
     return (
-      <nav className="p-5 bg-white shadow md:flex md:items-center md:justify-between">
-        <div>
+      <nav className="p-4 bg-white shadow md:flex md:items-center md:justify-between">
+        <div className="flex justify-between items-center">
           <span className="text-2xl font-sans font-bold cursor-pointer">
             Genie.
           </span>
         </div>
 
-        <ul className="md:flex md:items-center">
-          <li className="mx-4">
-            <a href="#" className="text-xl hover:text-slate-800 duration-500">Home</a>
-          </li>
-          <li className="mx-4">
-            <a href="#" className="text-xl hover:text-slate-800 duration-500">Product</a>
-          </li>
-          <li className="mx-4">
-            <a href="#" className="text-xl hover:text-slate-800 duration-500">About Us</a>
-          </li>
+        <div className="flex-1 flex justify-center">
+          <ul className="md:flex md:items-center space-x-8">
+            <li className="mx-4 my-6 md:my-0">
+              <a href="#" className="text-xl hover:text-slate-800 duration-500">Home</a>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <a href="#" className="text-xl hover:text-slate-800 duration-500">Product</a>
+            </li>
+            <li className="mx-4 my-6 md:my-0">
+              <a href="#" className="text-xl hover:text-slate-800 duration-500">About Us</a>
+            </li>
+          </ul>
+        </div>
 
-          <button className="bg-black text-white font-sans duratio-500 px-5 mx-4">
-            Get Started
-          </button>
-        </ul>
+        <div className="flex items-center space-x-4">
+          {getLinks()}
+        </div>
 
       </nav>
     );
