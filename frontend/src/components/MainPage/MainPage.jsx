494aa505c176efefa37820a45cabb2adc4650275
Dharani Yedavelly <dharani1414@gmail.com>
2024-06-10 08:36:22 -0700

    add titles for the landing page

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,9 +1,9 @@
 function MainPage() {
     return(
         <>
-            <div className="w-full flex flex-col justify-center">
-                <h1 className="text-9xl">AI-Powered Flashcards from your notes, made effortless</h1>
-                <h2>Create, Study, Explore, and Share: A Community of Flashcards</h2>
+            <div className="flex flex-col items-center font-sans">
+                <h1 className="font-semibold w-3/4 text-7xl text-center">AI-Powered Flashcards from your notes, made effortless</h1>
+                <h2 className="text-gray-600 pt-5">Create, Study, Explore, and Share: A Community of Flashcards</h2>
             </div>
         </>
     )
