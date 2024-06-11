6ff88f0da64439a8d8ab694623d8d687619d9b74
Edison Li <47466335+edison4354@users.noreply.github.com>
2024-06-10 21:24:27 -0700

    Update and refactor landing page components

---
+++ frontend/src/components/MainPage/Conversion.jsx
@@ -0,0 +1,30 @@
+import { useDispatch } from 'react-redux';
+import { openRegisterModal } from '../../store/modal';
+
+function Conversion() {
+    const dispatch = useDispatch();
+
+    const handleOpenRegisterModal = () => {
+        dispatch(openRegisterModal());
+    }
+
+    return(
+        <section id="conversion" className='mb-10 pt-40'>
+            <div className='flex flex-col justify-center items-center'>
+                <img className="w-80 -ml-2 relative animate-updown" src='../../../public/logo.svg'/>
+                <div className="pt-10 flex flex-col items-center font-sans mb-28 lg:px-40">
+                    <h1 className="font-bold text-5xl text-center leading-none mb-4">
+                        Ready to Transform Your Study Routine?
+                    </h1>
+                    <p className="text-gray-600 pb-5 text-2xl font-light tracking-wider text-center mb-4">
+                        Unlock the power of AI-generated flashcards and make studying more efficient with Genie. Join our 
+                        community of learners today and take the first step towards mastering your subjects with ease.
+                    </p>
+                    <button onClick={handleOpenRegisterModal} className="bg-black text-white font-medium text-lg px-8 py-2.5 rounded-lg">Sign Up for Free</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Conversion;
---
+++ frontend/src/components/MainPage/Product.jsx
@@ -0,0 +1,42 @@
+function Product() {
+    return(
+        <section id="product" className='py-20'>
+            <div className="flex flex-col items-center mb-20">
+                <h2 className="text-5xl font-medium mb-3 text-center">
+                    Transform Your Notes into Flashcards with <span className="font-extrabold">Genie.</span>
+                </h2>
+                <p className="w-2/3 text-gray-600 text-2xl font-light tracking-wider text-center mb-4">
+                    Users can create flashcard decks using AI, customize them to their liking, and search through existing decks created by other users.
+                </p>
+            </div>
+            <div className="mx-auto grid gap-6 lg:grid-cols-3 items-start max-w-none">
+
+                {/* 1st item */}
+                <div className="flex flex-col items-center p-6 bg-white rounded-xl border shadow-xl h-full">
+                    <img className="w-48 py-6" src="./../../../public/step1.jpg"/>
+                    <h4 className="text-xl font-bold mb-1.5">Step 1. Upload</h4>
+                    <p className="text-gray-600 text-center">Snap a photo, upload an image, or provide a PDF our AI will transform it into flashcards instantly!</p>
+                </div>
+
+                {/* 2nd item */}
+                <div className="flex flex-col items-center p-6 bg-white rounded-xl border shadow-xl">
+                    <img className="w-48 py-6" src="./../../../public/step1.jpg"/>
+                    <h4 className="text-xl font-bold mb-1.5">Step 2. Study</h4>
+                    <p className="text-gray-600 text-center">Review your flashcards, customize them as needed, and start studying using the flashcard study mode.</p>
+                </div>
+
+
+                {/* 3rd item */}
+                <div className="flex flex-col items-center p-6 bg-white rounded-xl border shadow-xl">
+                    <img className="w-48 py-6" src="./../../../public/step1.jpg"/>
+                    <h4 className="text-xl font-bold mb-1.5">Step 3. Share</h4>
+                    <p className="text-gray-600 text-center">Easily share your flashcards and explore flashcards shared by other users. Join a community of learners.</p>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Product;
