560a2f6666016bd84495dbe8002da37a673b6ee2
Jaspreet Singh <99jasingh@gmail.com>
2024-06-12 18:56:37 -0400

    Add Fav Icon

--- frontend/src/components/Dashboard/FlashcardSet.jsx
+++ frontend/src/components/Dashboard/FlashcardSet.jsx
@@ -1,8 +1,28 @@
-import { UserIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { UserIcon, HeartIcon as HeartOutline } from '@heroicons/react/24/outline';
+import { HeartIcon as HeartSolid } from '@heroicons/react/24/solid';
 
-const FlashcardSet = ({ title, termCount, username, onClick }) => {
+const FlashcardSet = ({ title, termCount, username, onClick, initialFav }) => {
+  const location = useLocation();
+  const [fav, setFav] = useState(initialFav);
+
+  const toggleFav = (e) => {
+    e.stopPropagation();
+    setFav(!fav);
+  };
+
   return (
-    <div onClick={onClick} className="max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-white cursor-pointer">
+    <div onClick={onClick} className="relative max-w-sm rounded-lg overflow-hidden shadow-lg p-4 bg-white cursor-pointer">
+      {location.pathname === '/explore' && (    //this line make sure that the heart is only visible in the /explore page
+        <div className="absolute top-2 right-2">
+          {fav ? (
+            <HeartSolid className="h-6 w-6 text-red-500" onClick={toggleFav} />    //this checks is the prop passed from the parent class is true then the heart will be red
+          ) : (
+            <HeartOutline className="h-6 w-6 text-gray-500" onClick={toggleFav} />  //else will be hert with empty inside
+          )}
+        </div>
+      )}
       <div className="font-bold text-xl mb-2">{title}</div>
       <div className="text-gray-700 text-base flex items-center space-x-2">
         <span>{termCount} terms</span>
